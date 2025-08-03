365c3f2f00f1492c2897c860612f97d8501db823
Wyndam Baxter <84729993+WyndamBaxter@users.noreply.github.com>
2025-08-02 21:16:20 -0300

    Novos exercícios adicionados

--- Estrutura-de-repeticao-10-11.py
+++ Estrutura-de-repeticao-10-11.py
@@ -1,3 +1,9 @@
+'''
+10  - Faça um programa que receba dois números inteiros
+ e gere os números inteiros que estão no intervalo compreendido por eles.
+11 - Altere o programa anterior para mostrar no final a soma dos números.
+'''
+
 def valida_entrada(mensagem):
   '''
   Função que valida a entrada do usuário.
---
+++ Estrutura-de-repeticao-12.py
@@ -0,0 +1,41 @@
+'''
+Desenvolva um gerador de tabuada, capaz de gerar a tabuada de
+qualquer número inteiro entre 1 a 10. O usuário deve informar de qual numero ele
+deseja ver a tabuada. A saída deve ser conforme o exemplo abaixo:
+
+Tabuada de 5:
+5 X 1 = 5
+5 X 2 = 10
+...
+5 X 10 = 50
+'''
+
+def valida_entrada(mensagem):
+    '''
+    Função que valida a entrada do usuário.
+    :param mensagem: mensagem do usuário.
+    :return: numero: número recebido pelo usuário.
+    '''
+    while True:
+        try:
+            numero = int(input(mensagem))
+            if 0 < numero <= 10:
+                return numero
+            else:
+                print("Por favor, digite um número entre 1 e 10")
+        except ValueError as ve:
+            print(f"Entrada inválida, por favor digite um número: {ve}")
+        except Exception as e:
+            print(f"Ocorreu um erro inesperado: {e}")
+
+def calcula_operacao(numero):
+    operacao = {}
+    for number in range(1, 11):
+        operacao[number] = number*numero
+    return operacao
+numero = valida_entrada("Digite um número entre 1 e 10: ")
+resultado = calcula_operacao(numero)
+
+print(f"Tabuada do numero {numero}")
+for multiplicador, resultado in resultado.items():
+    print(f"{numero} X {multiplicador} = {resultado}")
